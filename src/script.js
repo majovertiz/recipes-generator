70b899990094363db113f3fdad9c2f00788bd124
majovertiz <maria.jose.vertiz@gmail.com>
2023-12-20 21:55:04 -0800

    Added basic form handling and added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings:
+      "These fluffy cinnamon vanilla cookies are a guilt-free treat with less sugar and calories.",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
