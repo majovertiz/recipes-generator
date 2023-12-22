52e021bbea0482a98e41ebfb804971df577bc735
majovertiz <maria.jose.vertiz@gmail.com>
2023-12-21 18:28:01 -0800

    Added the AI Api Integration

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings:
-      "These fluffy cinnamon vanilla cookies are a guilt-free treat with less sugar and calories.",
-    autoStart: true,
-    delay: 50,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "08245o634da0cf8cebb4c8f9c9141at9";
+  let prompt = `User instructions: Generate a healthy recipe about ${instructionsInput.value}`;
+  let context =
+    "You are a healthy cooking expert and love to write short and delicious recipes.Your mission is to generate a recipe in basic HTML.  Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating ");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context:${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
